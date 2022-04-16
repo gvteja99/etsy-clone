e19b28b20ae5e0657b2cf232cdbdaf51d36086d4
gvteja99 <gvteja99@gmail.com>
2022-04-16 16:13:20 -0700

    Pagination Added to purchase page

--- client/src/components/Purchases.js
+++ client/src/components/Purchases.js
@@ -9,46 +9,69 @@ import { selectUser } from "../features/userSlice";
 import Navbar from "./Navbar";
 import Hoverbar from "./Hoverbar";
 import "./CartItem.css";
+import Pagination from './Pagination';
 
 
 const Purchases = () => {
     const dispatch = useDispatch();
     const user = useSelector(selectUser);
-    const [finalAmount, setFinalAmount] = useState();
-
-    const [items, SetItems] = useState([]);
     const products = useSelector(getCart);
+
+    const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [postsPerPage,setPostsPerPage] = useState(5);
+
+    // localStorage.setItem("postsPerPage", 5);
+    // let postsPerPage = localStorage.getItem("postsPerPage");
+
+    // Get current posts
+
+    const qtyChangeHandler = (qty) => {
+
+        console.log("pagination");
+        console.log("qty",qty);
+
+        setPostsPerPage(qty);
+        
+        // // {currentPage === 1 ?(
+        // // window.location.pathname = "/purchase"):(<></>);}
+
+        // localStorage.setItem("postsPerPage", qty);
+        // postsPerPage = localStorage.getItem("postsPerPage");
+
+        console.log("postsPerPage",postsPerPage);};
 
     useEffect(() => { getCItems(); }, []);
 
     const getCItems = () => {
+        setLoading(true);
         Axios.get("http://localhost:4000/getPurchases/" + user.id).then(
             (response) => {
-                console.log(response.data.result);
+                //console.log(response.data.result);
+                setPosts(response.data.result);
                 dispatch(createCart(response.data.result));
                 if (response.data.success === true) {
+                    setLoading(false);
                 }
             });
     };
 
-    const getCartCount = () => {
-        return products.reduce((qty, item) => Number(item.qty) + qty, 0);
+    // Change page
+
+    console.log("currremt ", currentPage)
+    const indexOfLastPost = currentPage * postsPerPage;
+    const indexOfFirstPost = indexOfLastPost - postsPerPage;
+    const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+    console.log("current posts :", currentPosts)
+
+    const paginate = pageNumber => {
+        setCurrentPage(pageNumber);
+        console.log("pageNumber", pageNumber);
+        console.log('currentPage', currentPage);
+
     };
 
-    const getCartSubTotal = () => {
-        return products
-            .reduce((price, item) => price + item.itemId.itemPrice * item.qty, 0).toFixed(2);
-    };
-
-    const handleCheckOut = () => {
-        Axios.get("http://localhost:4000/purchase/" + user.id)
-            .then((response) => {
-                console.log("Purchased");
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    };
     return (
         <>
             <Navbar />
@@ -57,42 +80,37 @@ const Purchases = () => {
             <div className="cartscreen">
                 <div className="cartscreen__left">
                     <h2>Previous Purchases</h2>
-                    {products.length === 0 ? (<div> You have no previous purchases. <Link to="/">Go Back</Link> </div>) : (
-                        products.map((item) => (
-                            //   <CartItem
-                            //     key={item.product}
-                            //     item={item}
-                            //     getCartSubTotal={getCartSubTotal}
-                            //     getCartCount={getCartCount}
+                    <p>Number of purchases per page</p><select value={postsPerPage} onChange={(e) => qtyChangeHandler(e.target.value)}  >
+                        {[2, 5, 10].map((x) => (
+                            <option key={x} value={x}>
+                                {x}
+                            </option>
+                        ))}
+                    </select>
+                    {currentPosts.length === 0 ? (<div> You have no previous purchases. <Link to="/">Go Back</Link> </div>) : (
+                        currentPosts.map((item) => (
 
-                            //   />
                             <div className="cart_pag" style={{ display: "flex", width: "100%", height: "200px", }} >
                                 <div className="cartitem">
-                                <p className="cartitem__price">Order Number: {item._id}</p>
-                                <p className="cartitem__price">Purchased On: {item.updatedAt}</p>
-                                <Link to={`/product/${item.product}`} className="cartItem__name">
+                                    <p className="cartitem__price">Order Number: {item._id}</p>
+                                    <p className="cartitem__price">Purchased On: {item.updatedAt}</p>
+                                    <Link to={`/product/${item.product}`} className="cartItem__name">
                                         <p>{item.itemId.itemName}</p>
                                     </Link>
                                     <div className="cartitem__image">
                                         <img src={"/Images/" + item.itemId.itemImage} alt={item.itemId.itemName} width={150} height={100} />
                                     </div>
-                                    
+
                                     <p className="cartitem__price">${item.itemId.itemPrice}</p>
-                                    
-                                    {/* <select value={item.qty} onChange={(e) => qtyChangeHandler(e.target.value)} className="cartItem__select" >
-                                        {[...Array(item.itemId.itemCount).keys()].map((x) => (
-                                            <option key={x + 1} value={x + 1}>
-                                                {x + 1}
-                                            </option>
-                                        ))}
-                                    </select> */}
-                                    {/* <button className="cartItem__deleteBtn" onClick={() => removeHandler(item._id)} > <Delete /> </button> */}
                                 </div>
                             </div>
                         ))
                     )}
+                    <Pagination postsPerPage={postsPerPage} totalPosts={posts.length} paginate={paginate} />
                 </div>
+
             </div>
+
         </>
     );
 };
